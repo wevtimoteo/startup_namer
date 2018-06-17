5e39619a91cc3e24c9082dffaef88923327e69f6
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-06-16 23:08:03 -0300

    Introduce RandomWords widget

---
+++ lib/widgets/random_words.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:startup_namer/states/random_words_state.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
