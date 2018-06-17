700864df62d272f1f726feaf89458a4a71bf6ede
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-06-17 14:14:41 -0300

    Uses Stateful ListView for each item

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new RandomWords(),
-        )
-      )
+      title: 'Startup Name Generator',
+      home: new RandomWords()
     );
   }
 }
--- lib/states/random_words_state.dart
+++ lib/states/random_words_state.dart
@@ -1,12 +1,45 @@
-import 'package:english_words/english_words.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:startup_namer/widgets/random_words.dart';
+import 'package:english_words/english_words.dart';
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator')
+      ),
+      body: _buildSuggestions(),
+    );
+  }
 
-    return new Text(wordPair.asPascalCase);
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+
+        final index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
